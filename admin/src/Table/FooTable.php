ef499c74c65622e15caa8ed245465e60872d7c09
SebUbu <rogaseb@gmail.com>
2026-01-03 23:48:35 +0100

    Dodaj pliki SQL do instalacji, odinstalowania i aktualizacji komponentu

---
+++ admin/src/Table/FooTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace FooSpace\Component\Foobar\Administrator\Table;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Table\Table;
+use Joomla\Database\DatabaseInterface;
+
+class FooTable extends Table
+{
+    public function __construct(DatabaseInterface $db)
+    {
+        parent::__construct('#__foobar_foos', 'id', $db);
+
+        $this->setColumnAlias('published', 'state');
+    }
+}
